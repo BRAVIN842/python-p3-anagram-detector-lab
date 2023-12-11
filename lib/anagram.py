0e4194e5239596bf576b6adbe13940234b5e17fb
Bravin <onsasebravin853@gmail.com>
2023-12-11 12:58:43 +0300

    Complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        return [w for w in word_list if self.is_anagram(w)]
+
+    def is_anagram(self, other_word):
+        other_word_lower = other_word.lower()
+        return other_word_lower != self.word and sorted(other_word_lower) == sorted(self.word)
